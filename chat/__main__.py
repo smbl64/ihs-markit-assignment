f6052e2c7149273854dec5bc75d3270d33e14a24
Mohammad Banisaeid <smbl64@gmail.com>
2021-05-13 14:52:03 +0200

    Add logging config

--- chat/__main__.py
+++ chat/__main__.py
@@ -1,3 +1,8 @@
+import logging
+
 from .server import start
+
+log_format = "%(asctime)s [%(levelname)-7s] [%(name)-15s] %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=log_format)
 
 start()
