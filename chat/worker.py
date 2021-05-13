330cf3bee75bb848a7c6646da7bb484edfc0ee59
Mohammad Banisaeid <smbl64@gmail.com>
2021-05-13 15:00:57 +0200

    Encapsulate the worker process in its own class

--- chat/worker.py
+++ chat/worker.py
@@ -1,24 +1,42 @@
+import logging
 import multiprocessing as mp
 import os
 import threading
 import uuid
 
 
-def proc_func(job_queue: mp.Queue, result_queue: mp.Queue):
-    while True:
-        job = job_queue.get()
-        print(f"{os.getpid()}: got job {job['id']}")
+class JobWorker:
+    """
+    Encapsulate a multiprocessing process.
+    """
+    def __init__(self, job_queue, result_queue):
+        self._logger = logging.getLogger(f"Worker-{os.getpid()}")
+        self._job_queue = job_queue
+        self._result_queue = result_queue
+
+    def start(self):
+        p = mp.Process(target=self._process_entry)
+        p.start()
+
+    def _process_entry(self):
+        while True:
+            job = self._job_queue.get()
+            self._logger.debug("Got job %s", job["id"])
+            self._handle_job(job)
+
+    def _handle_job(self, job):
         func = job["func"]
         kwargs = job["kwargs"]
         ret_value = func(**kwargs)
         job_result = dict(
             worker_pid=os.getpid(), job_id=job["id"], return_value=ret_value
         )
-        result_queue.put(job_result)
+        self._result_queue.put(job_result)
 
 
 class JobManager:
     def __init__(self, number_of_workers: int = 4):
+        self._logger = logging.getLogger("JobManager")
         self._job_queue = mp.Queue()
         self._result_queue = mp.Queue()
         self._job_id_to_callback_map = dict()
@@ -33,23 +51,29 @@ class JobManager:
 
     def _start_worker_processes(self, num_workers: int) -> None:
         for i in range(num_workers):
-            p = mp.Process(target=proc_func, args=(self._job_queue, self._result_queue))
-            p.start()
+            worker = JobWorker(self._job_queue, self._result_queue)
+            worker.start()
 
     def _result_monitor_thread_entry(self) -> None:
         while True:
             job_result = self._result_queue.get()
 
             job_id = job_result["job_id"]
-            callback_details = self._job_id_to_callback_map.get(job_id, None)
-            if not callback_details:
-                print("Didn't find the callback!!")
-                continue
-
-            callback_func = callback_details["func"]
-            callback_kwargs = callback_details["kwargs"]
             return_value = job_result["return_value"]
-            callback_func(return_value, **callback_kwargs)
+            try:
+                self._run_callback(job_id, return_value)
+            except Exception:
+                self._logger.exception("Failed to run callback")
+
+    def _run_callback(self, job_id, return_value):
+        callback_details = self._job_id_to_callback_map.pop(job_id, None)
+        if not callback_details:
+            self._logger.error("Didn't find the callback for %s", job_id)
+            return
+
+        callback_func = callback_details["func"]
+        callback_kwargs = callback_details["kwargs"]
+        callback_func(return_value, **callback_kwargs)
 
     def enqueue_job(
         self, *, job_func, job_func_kwargs, callback_func, callback_func_kwargs
@@ -65,20 +89,3 @@ class JobManager:
             "kwargs": callback_func_kwargs
         }
         self._job_queue.put(job_description)
-
-
-def start():
-    import helpers
-    manager = JobManager()
-
-    def my_callback(*args):
-        print("in callback --->", *args)
-
-    for x in range(5, 15):
-        manager.enqueue_job(
-            job_func=helpers.fibonacci, callback_func=my_callback, params={"n": x}
-        )
-
-
-if __name__ == "__main__":
-    start()
